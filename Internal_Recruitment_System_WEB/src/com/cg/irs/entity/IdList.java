8e1b6f61ad7759ce7bcc9e8c18d62e8d767a3456
sarita <saritasakhare31@gmail.com>
2018-08-02 10:39:19 +0530

    rmge some functions done

---
+++ Internal_Recruitment_System_WEB/src/com/cg/irs/entity/IdList.java
@@ -0,0 +1,19 @@
+package com.cg.irs.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IdList {
+
+	List<String> list = new ArrayList<String>();
+
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+	
+	
+}
